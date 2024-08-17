0ff4c0fceefe5b4c27f76b4128f86092e860ce33
Limo <mail@vogel-julian.de>
2024-08-18 00:57:14 +0200

    Added UniteTheNorth Project

---
+++ UniteTheNorth/UniteTheNorth.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+
+namespace UniteTheNorth;
+
+public class UniteTheNorth : MelonMod
+{
+    public static MelonLogger.Instance? Logger { get; private set; }
+    public const string Version = "0.1.0";
+
+    public override void OnInitializeMelon()
+    {
+        Logger = LoggerInstance;
+    }
+}
