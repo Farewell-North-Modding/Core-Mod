be064001d426552d2e3a9b420d7aa723f3f7a83b
Limo <mail@vogel-julian.de>
2024-08-18 01:15:04 +0200

    Added First UniteTheNorth Stuff

---
+++ UniteTheNorth/Networking/ServerBound/UserInformationPacket.cs
@@ -0,0 +1,16 @@
+﻿using MessagePack;
+
+namespace UniteTheNorth.Networking.ServerBound;
+
+[MessagePackObject]
+public class UserInformationPacket
+{
+    [Key(0)] public readonly string Version;
+    [Key(1)] public readonly string Username;
+
+    public UserInformationPacket(string username)
+    {
+        Version = UniteTheNorth.Version;
+        Username = username;
+    }
+}
