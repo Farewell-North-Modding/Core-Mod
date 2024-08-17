70821209941f56ca0b7b501844668eb596238691
Limo <mail@vogel-julian.de>
2024-08-18 00:57:34 +0200

    Added Assembly Info

---
+++ UniteTheNorth/obj/Debug/net6.0/UniteTheNorth.AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle("FarewellCore")]
+[assembly: AssemblyDescription("A core mod for the game Farewell North")]
+[assembly: AssemblyProduct("FarewellCore")]
+[assembly: AssemblyCopyright("Created by Limo")]
+[assembly: AssemblyVersion(UniteTheNorth.UniteTheNorth.Version)]
+[assembly: AssemblyFileVersion(UniteTheNorth.UniteTheNorth.Version)]
+
+[assembly: MelonInfo(typeof(UniteTheNorth.UniteTheNorth), "FarewellCore", UniteTheNorth.UniteTheNorth.Version, "Limo")]
+[assembly: MelonGame("Kyle Banks", "Farewell North")]
