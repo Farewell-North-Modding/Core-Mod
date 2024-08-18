060868a14dab09b1947917cf15d93a4d6385be14
Limo <mail@vogel-julian.de>
2024-08-18 15:39:28 +0200

    Fixed Unite the North AssemblyInfo.cs

--- UniteTheNorth/obj/Debug/net6.0/UniteTheNorth.AssemblyInfo.cs
+++ UniteTheNorth/obj/Debug/net6.0/UniteTheNorth.AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 using MelonLoader;
 
-[assembly: AssemblyTitle("FarewellCore")]
-[assembly: AssemblyDescription("A core mod for the game Farewell North")]
-[assembly: AssemblyProduct("FarewellCore")]
+[assembly: AssemblyTitle("UniteTheNorth")]
+[assembly: AssemblyDescription("A mod aiming at adding a multiplayer to Farewell North")]
+[assembly: AssemblyProduct("Unite the North")]
 [assembly: AssemblyCopyright("Created by Limo")]
 [assembly: AssemblyVersion(UniteTheNorth.UniteTheNorth.Version)]
 [assembly: AssemblyFileVersion(UniteTheNorth.UniteTheNorth.Version)]
 
-[assembly: MelonInfo(typeof(UniteTheNorth.UniteTheNorth), "FarewellCore", UniteTheNorth.UniteTheNorth.Version, "Limo")]
+[assembly: MelonInfo(typeof(UniteTheNorth.UniteTheNorth), "Unite the North", UniteTheNorth.UniteTheNorth.Version, "Limo")]
 [assembly: MelonGame("Kyle Banks", "Farewell North")]
