1f4ffcbbd07290e397d7dd20ff9b992d3855248a
Limo <mail@vogel-julian.de>
2024-08-18 15:11:36 +0200

    Added basic NetPlayer

---
+++ UniteTheNorth/Systems/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using FarewellCore.Tools;
+using UniteTheNorth.Tools;
+
+namespace UniteTheNorth.Systems;
+
+public static class PlayerManager
+{
+    public static NetPlayer? CreateNetPlayer()
+    {
+        var newObject = GameplayFinder.FindPlayer()?.GetGameObject();
+        return newObject == null ? null : newObject.GetComponent<NetPlayer>();
+    }
+}
---
+++ UniteTheNorth/Tools/NetPlayer.cs
@@ -0,0 +1,35 @@
+﻿using FarewellCore.Wrapper;
+using MelonLoader;
+using UnityEngine;
+
+namespace UniteTheNorth.Tools;
+
+[RegisterTypeInIl2Cpp]
+public class NetPlayer : MonoBehaviour
+{
+    private PlayerWrapper? _wrapper;
+    public Vector3 moveToPosition;
+    public float speed = 5F;
+
+    private void Start()
+    {
+        _wrapper = new PlayerWrapper(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, moveToPosition) < .5F)
+        {
+            _wrapper?.Animal.StopMoving();
+            return;
+        }
+        var direction = (moveToPosition - transform.position).normalized;
+        _wrapper?.Animal.Move(direction);
+        transform.position = Vector3.Lerp(transform.position, moveToPosition, speed * Time.deltaTime);
+    }
+
+    public void UpdatePosition(Vector3 position)
+    {
+        moveToPosition = position;
+    }
+}
