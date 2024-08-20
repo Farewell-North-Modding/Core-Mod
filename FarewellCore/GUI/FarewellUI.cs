e965018439958b83fa3a53f7e96d17c421df5158
Limo <mail@vogel-julian.de>
2024-08-20 03:03:56 +0200

    Finished basic UI and Panel

--- FarewellCore/GUI/FarewellUI.cs
+++ FarewellCore/GUI/FarewellUI.cs
@@ -1,6 +1,28 @@
-﻿namespace FarewellCore.GUI;
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class FarewellUI
+namespace FarewellCore.GUI;
+
+[RegisterTypeInIl2Cpp]
+public class FarewellUI : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        var canvasScaler = gameObject.AddComponent<CanvasScaler>();
+        canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        canvasScaler.referenceResolution = new Vector2(1920, 1080);
+        gameObject.AddComponent<HorizontalLayoutGroup>();
+    }
+
+    public static FarewellUI Create(string name)
+    {
+        var go = new GameObject(name);
+        var ui = go.AddComponent<FarewellUI>();
+        var canvas = go.AddComponent<Canvas>();
+        canvas.sortingOrder = 20000;
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        go.AddComponent<GraphicRaycaster>();
+        return ui;
+    }
 }
