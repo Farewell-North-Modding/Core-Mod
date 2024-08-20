65d688f29d599e18c21e6b4755567602dbadb68c
Limo <mail@vogel-julian.de>
2024-08-21 00:10:44 +0200

    Added titlescreen-replace

--- UniteTheNorth/GUI/TitleScreenPatcher.cs
+++ UniteTheNorth/GUI/TitleScreenPatcher.cs
@@ -1,5 +1,7 @@
 ﻿using MelonLoader;
+using MelonLoader.Utils;
 using UnityEngine;
+using UnityEngine.UI;
 using UniverseLib.UI;
 
 namespace UniteTheNorth.GUI;
@@ -7,6 +9,8 @@ namespace UniteTheNorth.GUI;
 [RegisterTypeInIl2Cpp]
 public class TitleScreenPatcher : MonoBehaviour
 {
+    private const string LogoLocation = "/UniteTheNorth/UniteTheNorth.png";
+    
     public static void Patch(string sceneName)
     {
         if(sceneName != "Main Menu")
@@ -23,7 +27,22 @@ public class TitleScreenPatcher : MonoBehaviour
 
     private static void ReplaceArtwork()
     {
-        // TODO: Create Replacer
+        var fullPath = MelonEnvironment.ModsDirectory + LogoLocation;
+        if (!File.Exists(fullPath))
+        {
+            UniteTheNorth.Logger.Warning("Couldn't find custom title screen artwork");
+            return;
+        }
+        var data = File.ReadAllBytes(fullPath);
+        var texture = new Texture2D(2, 2);
+        texture.LoadImage(data);
+        var rect = new Rect(0, 0, texture.width, texture.height);
+        var sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+        var obj = GameObject.Find("Logo");
+        if(obj == null)
+            UniteTheNorth.Logger.Warning("Couldn't find logo game object");
+        else
+            obj.GetComponent<Image>().sprite = sprite;
     }
 
     private void AddConnectUI()
